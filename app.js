26bd23da56a70d471ddcdc36b3a9d2039335e88a
MethSarcus <seth@smarcus.dev>
2022-03-04 17:17:14 -0500

    delted thing

--- app.js
+++ app.js
@@ -8,20 +8,6 @@ app.use(express.urlencoded({
 
 app.use(cors());
 
-
-const firebaseConfig = {
-    apiKey: "AIzaSyDpY3SEp6Z2ts7mYuTEVc32-kfPQunVemw",
-    authDomain: "goodluckhavecheese.firebaseapp.com",
-    projectId: "goodluckhavecheese",
-    storageBucket: "goodluckhavecheese.appspot.com",
-    messagingSenderId: "389835653569",
-    appId: "1:389835653569:web:72770b7fdd1915cf906f40",
-    measurementId: "G-V47YP85PN6"
-  };
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
 const testData = {race: "protoss", glhf: false, cheese: true};
 
 const PORT = process.env.PORT || 8080;
