4f1b511af3da837e16c7859a7882f3a3137cdad6
MethSarcus <seth@smarcus.dev>
2022-03-05 18:01:02 -0500

    did stuff

--- server/app.js
+++ server/app.js
@@ -65,5 +65,10 @@ app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "/client/build", "index.html"));
 });
 
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
+
 
 
