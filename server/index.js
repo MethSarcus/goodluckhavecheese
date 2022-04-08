6a05c4411c4d427b94f947b24e40915137790fd8
MethSarcus <seth@smarcus.dev>
2022-04-08 14:22:00 -0400

    Changed shit around

--- server/index.js
+++ server/index.js
@@ -61,8 +61,10 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 //     client.close();
 // });
 
+app.use(express.static(path.resolve(__dirname, "../client/build")));
+
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "/client/build", "index.html"));
+  res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
 });
 
 // Serve up static assets (usually on heroku)
