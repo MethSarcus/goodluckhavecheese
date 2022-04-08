e1704df6e4585d42ea7baa95935598dbbb75838a
Seth Marcus <seth@smarcus.dev>
2022-04-08 16:20:52 -0400

    changed port again

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ app.use(express.urlencoded({
 
 app.use(cors());
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
   
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
 
