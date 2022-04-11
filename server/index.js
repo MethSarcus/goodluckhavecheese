aa4c9f717f8f02045fc4bb65303f1fbbc2d8285a
Seth Marcus <seth@smarcus.dev>
2022-04-11 12:31:29 -0400

    changed db line

--- server/index.js
+++ server/index.js
@@ -35,7 +35,7 @@ app.listen(PORT, console.log(`Server started on port ${PORT}`));
 
 const { MongoClient } = require('mongodb');
 const uri = "mongodb+srv://methsarcus:LC25g9IwqrE2r1mM@cheesestats.vtr92.mongodb.net/CheeseStats?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
 
 // Handle GET requests to /api route
 // app.get("/api", (req, res) => {
@@ -56,6 +56,7 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
       let glhf = req.body.glhf;
       let cheese = req.body.cheese;
       let dataToInsert = {race: race, glhf: glhf, cheese: cheese};
+      const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
       client.connect(err => {
         const collection = client.db("cheeseDB").collection("stats");
         // perform actions on the collection object
