eff9e7033f4b9294303efededdca16c15549f215
Seth Marcus <seth@smarcus.dev>
2022-04-11 15:01:09 -0400

    Added validation

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require('path');
 const cors = require('cors');
+const { body, validationResult } = require('express-validator');
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({
@@ -36,25 +37,16 @@ app.listen(PORT, console.log(`Server started on port ${PORT}`));
 const { MongoClient } = require('mongodb');
 const uri = "mongodb+srv://methsarcus:LC25g9IwqrE2r1mM@cheesestats.vtr92.mongodb.net/CheeseStats?retryWrites=true&w=majority";
 
+  app.post('/', body('race').isString(), body('glhf').isBoolean(), body('cheese').isBoolean(), (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
 
-// Handle GET requests to /api route
-// app.get("/api", (req, res) => {
-//     client.connect(err => {
-//         const collection = client.db("cheeseDB").collection("stats");
-//         // perform actions on the collection object
-//         collection.insertOne(testData, (err, data) => {
-//           if(err) return console.log(err);
-//           res.send(('saved to db: ' + data));
-//           client.close();
-//       });
-//       });
-//     //res.json({ message: "Hello from server!" });
-//   });
-
-  app.post('/', (req, res) => {
       let race = req.body.race;
       let glhf = req.body.glhf;
       let cheese = req.body.cheese;
+      
       let dataToInsert = {race: race, glhf: glhf, cheese: cheese};
       const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
       client.connect(err => {
@@ -67,28 +59,6 @@ const uri = "mongodb+srv://methsarcus:LC25g9IwqrE2r1mM@cheesestats.vtr92.mongodb
       });
       });
   });
-
-//   app.post('/', (req, res) => {
-//     let race = req.body.race;
-//     let glhf = req.body.glhf;
-//     let cheese = req.body.cheese;
-//     let dataToInsert = {race: race, glhf: glhf, cheese: cheese, success: true};
-//     res.send(dataToInsert);
-// });
-
-// client.connect(err => {
-//     const collection = client.db("cheeseDB").collection("stats");
-
-//     app.post('/track', (req, res) => {
-//     console.log(req.body);
-//     collection.insertOne(req.body, (err, data) => {
-//         if(err) return console.log(err);
-//         res.send(('saved to db: ' + data));
-//     })
-// });
-//     // perform actions on the collection object
-//     client.close();
-// });
 
 app.use(express.static(path.resolve(__dirname, "../client/build")));
 
